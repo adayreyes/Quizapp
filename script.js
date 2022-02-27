ea115e0a886f18fb49e6c3769503ccd9e73b8a0b
Aday Reyes <adayreyes4a@gmail.com>
2022-02-27 19:26:20 +0100

    start-card

---
+++ script.js
@@ -0,0 +1,8 @@
+function start(){
+   let start =  document.getElementById('start-card');
+   start.classList.add("animation");
+   setTimeout(() => {start.style.display = "none";},1500)
+   
+   
+   
+}
