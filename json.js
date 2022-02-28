cf9f57ad041bec7e866f79f39a0929fd056170b9
Aday Reyes <adayreyes4a@gmail.com>
2022-02-28 20:07:35 +0100

    render function

---
+++ json.js
@@ -0,0 +1,42 @@
+let questions = [
+    {
+        "question":"Wie sagt man Rücksack auf Spanisch?",
+        "answer_1":"Mochila",
+        "answer_2":"Mochilo",
+        "answer_3":"Balleta",
+        "answer_4":"Riñonera ",
+        "correct":"3" 
+    },
+    {
+        "question":"Wie sagt man Regal auf Spanisch?",
+        "answer_1":"Regadera",
+        "answer_2":"Rollo",
+        "answer_3":"Encimera",
+        "answer_4":"Estanteria",
+        "correct":"2" 
+    },
+    {
+        "question":"Wie sagt man Spatz auf Spanisch?",
+        "answer_1":"Golondrina",
+        "answer_2":"Gorrion",
+        "answer_3":"Gorrino",
+        "answer_4":"Garza",
+        "correct":"2" 
+    },
+    {
+        "question":"Wie sagt man Notizblock auf Spanisch?",
+        "answer_1":"Auriculares",
+        "answer_2":"Agenda",
+        "answer_3":"Bloqueador",
+        "answer_4":"Cuaderno",
+        "correct":"2" 
+    },
+    {
+        "question":"Wie sagt man Spiegel auf Spanisch?",
+        "answer_1":"Cuadro",
+        "answer_2":"Espejo",
+        "answer_3":"Reflejo",
+        "answer_4":"Reflectador",
+        "correct":"2" 
+    }
+]
--- script.js
+++ script.js
@@ -1,8 +1,99 @@
-function start(){
-   let start =  document.getElementById('start-card');
+let i = 0;
+
+function getById(id) {
+   return document.getElementById(id);
+}
+
+function start() {
+   let start = document.getElementById("start-card");
    start.classList.add("animation");
-   setTimeout(() => {start.style.display = "none";},1500)
-   
-   
-   
+   setTimeout(() => {start.style.display = "none"},1500)
+}
+
+function render() {
+   let main_card = getById("main-card");
+   main_card.innerHTML = "";
+   renderQuestion();
+}
+
+function renderCard(template){
+   let element = getById("main-card");
+   element.innerHTML += template
+}
+
+function renderQuestion() {
+  renderCard(cardHeaderTemplate());
+  renderCard(cardBodyTemplate());
+  renderCard(cardFooterTemplate());
+  
+}
+
+function nextQuestion(){
+   i++;
+   if(i < questions.length){
+      render()
+   } else{
+      alert("FINITO")
+   }
+}
+
+function cardHeaderTemplate(){
+   return`<div class="card-header">
+   <h2 class="blue-txt">Quizmaster</h2>
+   <h4 id="question">
+      ${questions[i]['question']}
+   </h4>
+ </div>`
+}
+
+function cardBodyTemplate(){
+   return` <div class="card-body">
+      ${answer1Template()}${answer2Template()}${answer3Template()}${answer4Template()}
+      </div>`
+}
+
+function answer1Template(){
+   return `<div class="card">
+   <div class="card-body" id="answer-1">
+   <span class="letter-box">A</span>
+      <span>${questions[i]['answer_1']}</span>
+      </div>
+ </div>`
+}
+
+function answer2Template(){
+   return `<div class="card">
+      <div class="card-body" id="answer-2">
+      <span class="letter-box">B</span>
+      <span>${questions[i]['answer_2']}</span>
+      </div>
+ </div>`
+}
+
+function answer3Template(){
+   return `<div class="card">
+      <div class="card-body" id="answer-3">
+      <span class="letter-box">C</span>
+      <span>${questions[i]['answer_3']}</span>
+      </div>
+ </div>`
+}
+
+function answer4Template(){
+   return `<div class="card">
+      <div class="card-body" id="answer-4">
+      <span class="letter-box">D</span>
+      <span>${questions[i]['answer_4']}</span>
+      </div>
+      </div>`
+}
+
+function cardFooterTemplate(){
+   return`<div class="card-footer">
+   <div class="questions-number">
+     <span id="question-number">Frage ${i + 1}</span>
+     <span>von ${questions.length}</span>
+   </div>
+   <button type="button" class="btn btn-primary btn-lg" onclick="nextQuestion()">Next</button>
+ </div>`
 }
